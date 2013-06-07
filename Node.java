d95c30b83b34b4ca51eaaecb3d021c5adae404ef
dtbulmer <dtbulmer2@gmail.com>
2013-06-06 22:25:55 -0600

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,41 +1,82 @@
 
 
+
+
 import java.util.ArrayList;
+
 import java.util.Iterator;
 
+
+
 public class Node {
-	public String data;
+
+	public char data;
+
 	public boolean isLit;
+
 	public boolean isDirty;
+
 	
-	public Node(String data){
+
+	public Node(char data){
+
 		this.data = data;
+
 		isLit = false;
+
 		isDirty = false;
+
 	}
+
 	
+
 	public boolean getisLit(){
+
 		return isLit;
+
 	}
+
 	
-	public String getData(){
+
+	public char getData(){
+
 		return data;
+
 	}
+
 	
+
 	public void setisLit(boolean isLit){
+
 		this.isLit = isLit;
+
 	}
+
 	
+
 	public boolean getisDirty()
+
 	{
+
 		return isDirty;
+
 	}
+
 	
+
 	public void setisDirty(boolean isDirty)
+
 	{
+
 		this.isDirty = isDirty;
+
 	}
+
 }
+
+
+
+
 
 
 
