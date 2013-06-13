247f0587a1c3e9c36dc0763f25a68c28e58b86f4
dtbulmer <dtbulmer2@gmail.com>
2013-06-12 21:22:17 -0600

    Update Labyrinth.java
    last time i put Minotaur here by accident

--- Labyrinth.java
+++ Labyrinth.java
@@ -1,201 +1,81 @@
-import java.util.ArrayList;
+import java.util.*;
+
+public class Labyrinth {
 
 
 
-/**
-
- * 
-
- */
-
-
-
-/**
-
- * @author Derrik2
-
- *
-
- */
-
-public class Minotaur 
-
-{
-
-	ArrayList<Edge> M; 
-
-	Labyrinth graph;
-
-	boolean coward;
+	private Map<Node, ArrayList<Edge>> nodes;
 
 	
 
-	public Minotaur(Labyrinth graph, Node start, Node end)
+	
 
-	{
+	public Labyrinth(){
 
-		M = new ArrayList<Edge>();
+		this.nodes = new HashMap<Node, ArrayList<Edge>>(); 
 
-		this.graph = graph;
+	}
 
-		coward = false;
 
-		// make an Edge
 
-		Edge starting = new Edge(start, end);
+	public void addNode(Node node, Node[] edges) {
 
-		
+		Edge e = null;
 
-		ArrayList<Edge> listStart = this.graph.getNodes().get(start);
+		ArrayList<Edge> list = new ArrayList<Edge>();
 
-		
+	    for (Node edge : edges){
 
-		for(Edge edge : listStart)
+	    	e = new Edge(node, edge);
 
-		{
+	    	list.add(e);
 
-			if(edge.getNode1().getData() == start.getData()
+	    }
 
-					&& edge.getNode2().getData() == end.getData())
+	   nodes.put(node, list);
 
-			{
+	  }
 
-				// then we found it
+	
 
-				M.add(edge); 
+	public void print() {
+
+		String result = "";
+
+		for (Node v : nodes.keySet()) {
+
+		    ArrayList<Edge> node = nodes.get(v);
+
+		    Iterator<Edge> iterator = node.iterator();
+
+		    System.out.print(v.getData() + " : ");
+
+		    while(iterator.hasNext()){
+
+				result += "" +iterator.next().getNode2().getData();
 
 			}
 
-		}
+		    System.out.println(result);
 
-		if(M.isEmpty())
-
-		{
-
-			System.out.println("Error, not a valid start position");
-
-		}
-
-		// find and confirm start position in graph
-
-		
-
-	}
-
-		
-
-	public void move()
-
-	{
-
-		 ArrayList<Edge> list;
-
-		 Edge n = M.get(M.size() - 1);
-
-		 Node prev = n.getNode1();
-
-		 Node curr = n.getNode2();
-
-		 list = graph.getNodes().get(curr);
-
-		 int i = list.size() - 1;
-
-		 if(!coward)
-
-		 {
-
-			 while(prev.getData() != list.get(i).getNode2().getData())
-
-			 {
-
-				 i--;
-
-			 }
-
-		 
-
-			 if(!list.get(list.size() - 1).getNode2().getisLit())
-
-			 {
-
-				 if(i <= 0)
-
-				 	M.add(list.get(list.size() - 1));
-
-				 else
-
-				 	M.add(list.get(i - 1));
-
-				 curr.setisDirty(true);
-
-			 }
-
-			 else
-
-			 {
-
-				 // he runs like a coward back through the tunnel
-
-				 if(i <= 0)
-
-					 M.add(list.get(list.size() - 1));
-
-				 else
-
-					 M.add(list.get(i - 1));
-
-				 coward = true;
-
-				 
-
-			 }
+		    result = "";
 
 		 }
 
-		 else
-
-		 {
-
-			 coward = false;
-
-			 list = graph.getNodes().get(curr);
-
-			 while(prev.getData() != list.get(i).getNode2().getData())
-
-			 {
-
-				 i--;
-
-			 }
-
-		 
-
-			 if(i <= 0)
-
-			 	M.add(list.get(0));
-
-			 else
-
-			 	M.add(list.get(i));
-
-		 }
-
-		 
-
-		 
-
-		 
-
-	}
+	 }
 
 	
 
-	public ArrayList<Edge> getM()
+	public Map<Node, ArrayList<Edge>> getNodes()
 
 	{
 
-		return M;
+		return nodes;
 
 	}
 
 }
+
+
+
+
